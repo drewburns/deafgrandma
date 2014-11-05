47fdaa0a3aec6d69afd9048ee08d731459bc493e
drewburns <drewburnsbab@gmail.com>
2014-11-05 11:45:14 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -7,12 +7,11 @@ ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'sqlite3:test1.db
 get '/' do
 	erb :index
 end
-@bye_times = 0
-post(@bye_times) '/' do
+
+post '/' do
 	@input = params[:user_input]
 	if @input == "BYE"
 		@reply = "No Stay!"
-		@bye_times += 1
 	elsif @input == @input.upcase
 		@reply = "NO, NOT SINCE 19#{rand(21)+30}!"		
 	else

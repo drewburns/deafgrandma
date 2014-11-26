2de6f2aef1cb53414fb0893681f46be0d32cb9db
drewburns <andrewburns@Andrews-MacBook-Pro.local>
2014-11-26 10:28:38 -0500

    Added CSS and a background

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){                                  // this means, only do this when the document  is ready
   $("#user_input").on("submit", function(event) {     // do this function when element w/ id user_input is submitted
-    event.preventDefault();                                        // stop whatever would happen by default (don't submit the form)
+    event.preventDefault();                                       // stop whatever would happen by default (don't submit the form)
 
     var url = $(this).attr('action')
     console.log(url)                            // i like to save things to variables. this one is grabbing the URL from the form
@@ -8,8 +8,11 @@ $(document).ready(function(){                                  // this means, on
     console.log(data)                        // serialize the content from 'this' (the form)
     
     $.post(url, data, function(response){
-    console.log(response)            // you can use $.ajax or $.post. it submits a post requst with the specified url and data
-      $("#grandma-says").html(response)           // and get a response. this line takes the response and puts it in the html of element with id = 
+    console.log(response)
+       $("#grandma-says").fadeIn()      // you can use $.ajax or $.post. it submits a post requst with the specified url and data
+      $("#grandma-says").html(response)
+      $("#grandma-says").fadeOut(3000)
+        // and get a response. this line takes the response and puts it in the html of element with id = 
                                                                         //  grandma-says
     });
   });
